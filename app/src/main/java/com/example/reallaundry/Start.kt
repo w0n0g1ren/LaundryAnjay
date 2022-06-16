41b0696fcd0401a8811aaf5e1008896f2ed03491
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2022-06-17 00:58:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/reallaundry/Start.kt
@@ -0,0 +1,22 @@
+package com.example.reallaundry
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Start : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent (this,DashboardActivity::class.java)
+            startActivity(intent)
+            onDestroy()
+        },5000)
+
+    }
+}
